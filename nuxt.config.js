4accb836a6e6d458330c96284a00467d2d14f914
nana1177 <pakapakabanana@gmail.com>
2022-04-29 16:13:08 +0900

    add: ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,15 +7,22 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'play',
+    title: '栄東祭2022　公式ウェブサイト',
     htmlAttrs: {
       lang: 'jp'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { hid: 'description', name: 'description', content: '栄東祭2022の公式ウェブサイトです。' },
+      { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'og:site_name', property: 'og:site_name', content: '栄東祭2022　公式ウェブサイト' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://sh-fes.com/' },
+      { hid: 'og:title', property: 'og:title', content: '栄東祭2022　公式ウェブサイト' },
+      { hid: 'og:description', property: 'og:description', content: '栄東祭2022の公式ウェブサイトです。' },
+      { hid: 'og:image', property: 'og:image', content: 'https://sh-fes.com/logo.png' },
+      { name: 'twitter:card', content: 'summary' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
