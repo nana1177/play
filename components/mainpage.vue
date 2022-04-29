b0bb16a565d1768c6244d7edd16ea7c1d0d5255c
nana1177 <pakapakabanana@gmail.com>
2022-04-29 15:56:20 +0900

    delete: header

--- components/mainpage.vue
+++ components/mainpage.vue
@@ -1,26 +1,5 @@
 <template>
 <div class=all>
-  <header class="header">
-    <NuxtLink
-    to=""
-    tag="div">
-      <h1 class="sakae-home">
-        栄東祭
-      </h1>
-    </NuxtLink>
-    <NuxtLink
-    to="about"
-    tag="div"
-    class="about-button">
-      About
-    </NuxtLink>
-    <NuxtLink
-    to="news"
-    tag="div"
-    class="news-button">
-      News
-    </NuxtLink>
-  </header>
   <div class="main">
     <div class="titleWrapper">
     <h1 class="title">
@@ -35,6 +14,9 @@
     <div class="detail">
       <p class="time">
         開催日時
+      </p>
+      <p class="time-detail">
+        ９月１７日（土曜日）・１８日（日曜日）
       </p>
     </div>
     <div class="line">
@@ -62,83 +44,6 @@ export default {
     position: absolute;
     width: 100%;
   }
-  .header{
-    background-color: rgba(6, 12, 34, 0.8);
-    position: fixed;
-    top: 0px;
-    width: 100%;
-    height: 90px;
-    z-index: 997;
-  }
-  .sakae-home{
-    cursor: pointer;
-    cursor: hand;
-    color: rgb(255, 255, 255);
-    font-size: 6.8vh;
-    position: absolute;
-    top: -4.3vh;
-    left: 3vh;
-  }
-  .about-button{
-    color: rgb(255, 255, 255);
-    opacity: .5;
-    font-size: 40px;
-    position: absolute;
-    display: inline-block;
-    text-decoration: none;
-    top: 4.3vh;
-    left: 800px;
-    cursor: pointer;
-  }
-  .about-button::after {
-    position: absolute;
-    bottom: 2px;
-    left: 0;
-    content: '';
-    width: 100%;
-    height: 2px;
-    background: rgb(255, 255, 255);
-    opacity: 0;
-    transition: .3s;
-  }
-  .about-button:hover {
-    opacity: 1;
-  }
-  .about-button:hover::after {
-    bottom: 5px;
-    opacity: 1;
-  }
-  .news-button{
-    color: rgba(255, 255, 255, 0.8);
-    opacity: .5;
-    font-size: 40px;
-    position: absolute;
-    display: inline-block;
-    text-decoration: none;
-    top: 4.3vh;
-    left: 950px;
-    cursor: pointer;
-  }
-  .news-button::after {
-    position: absolute;
-    bottom: 2px;
-    left: 0;
-    content: '';
-    width: 100%;
-    height: 2px;
-    background: rgb(255, 255, 255);
-    opacity: 0;
-    visibility: hidden;
-    transition: .3s;
-  }
-  .news-button:hover{
-    opacity: 1;
-  }
-  .news-button:hover::after {
-    bottom: 5px;
-    opacity: 1;
-    visibility: visible;
-  }
   .main{
     position: fixed;
     background: url(~@/static/school.jpg) top center;
@@ -146,7 +51,7 @@ export default {
     height: 100vh;
     background-size: cover;
     z-index: -10;
-    overflow: scroll;
+    overflow-y: scroll;
   }
   .main::before{
     content: "";
@@ -157,6 +62,9 @@ export default {
     left: 0;
     right: 0;
     z-index: -5;
+  }
+  .main::-webkit-scrollbar {
+    z-index: 999;
   }
   .titleWrapper {
     text-align: center;
@@ -219,5 +127,8 @@ export default {
     position: absolute;
     top: 170vh;
     left: 80vh;
+  }
+  .time-detail{
+    width: 
   }
 </style>
